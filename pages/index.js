9c95fe834205953e8f30570a0cf6d77409265a23
amaalswalkah-cell <amaalswalkah@gmail.com>
2026-01-12 01:03:20 +0300

    Add Home component for school website

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>الموقع الرسمي لمدرسة عين البيضاء الثانوية المختلطة</h1>
+      <p>مرحبًا بكم في موقعنا الإلكتروني حيث نشارك أخبار المدرسة، الأنشطة، والفعاليات.</p>
+      <img src="/logo.png" alt="شعار المدرسة" width="200" />
+    </div>
+  );
+  }
