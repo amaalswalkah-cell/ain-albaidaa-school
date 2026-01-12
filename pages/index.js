50a75debdc8d657b6f65a3fccd8e4263371eee1f
amaalswalkah-cell <amaalswalkah@gmail.com>
2026-01-12 03:30:04 +0300

    Update logo image source in Home component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
-export default function Home() {
+<img src="/logo.jpg" alt="شعار المدرسة" width="200" />export default function Home() {
   return (
     <div style={{ textAlign: 'center', padding: '2rem' }}>
       <h1>الموقع الرسمي لمدرسة عين البيضاء الثانوية المختلطة</h1>
       <p>مرحبًا بكم في موقعنا الإلكتروني حيث نشارك أخبار المدرسة، الأنشطة، والفعاليات.</p>
-      <img src="/logo.png" alt="شعار المدرسة" width="200" />
-    </div>
+<img src="/logo.jpg" alt="شعار المدرسة" width="200" />    </div>
   );
   }
